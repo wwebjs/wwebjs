71612231c96353b3a5a5724b53c32a4e9c25ec69
2hoch1 <mail@2hoch1.dev>
2026-03-27 23:07:40 +0100

    refactor: remove shared layout

--- apps/website/src/app/(examples)/layout.tsx
+++ apps/website/src/app/(examples)/layout.tsx
@@ -1,7 +1,5 @@
-import { baseOptions } from '@/lib/layout.shared'
-import { HomeLayout } from 'fumadocs-ui/layouts/home'
 import type { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>
+  return <div>{children}</div>
 }
